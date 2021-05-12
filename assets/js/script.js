a4d576f9a8f8c971ff053217ff17c526e5b012b1
Dan Høegh <dan.hoegh@gmail.com>
2021-05-12 12:32:20 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // JS by Dan Høegh
 // UCN MMD 2021
 
-let key = "DEMO_KEY"; // Replace this with your own key
+let key = "DEMO_KEY"; // Replace this with your own key, get one here: https://api.nasa.gov/
 let url = `https://api.nasa.gov/planetary/apod?api_key=${key}`;
 
